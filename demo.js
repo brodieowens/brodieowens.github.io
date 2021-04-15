d80646b14b67c19c0c98dff2ed2e9a6b150b8cb7
brodieowens <82548036+brodieowens@users.noreply.github.com>
2021-04-14 22:17:47 -0400

    Add files via upload

--- demo.js
+++ demo.js
@@ -2,7 +2,11 @@ window.addEventListener("DOMContentLoaded",() => {
 
 
     const myList = document.getElementById("my-list")
+    const catList = document.getElementById("cat-list")
+
     const submitButton = document.getElementById("submit-button")
+    const catButton = document.getElementById("cat-button")
+
     const listInput = document.getElementById("list-input")
  
     submitButton.addEventListener("click", (evt) => {
@@ -12,6 +16,17 @@ window.addEventListener("DOMContentLoaded",() => {
         myList.append(newItem)
     })
 
+    
 
+    async function newFact(evt) {
+        evt.preventDefault()
+        let response = await axios.get("https://cat-fact.herokuapp.com/facts")
+        let catFact = response.data[0].text
+        let newItem = document.createElement("li")
+        newItem.innerHTML= catFact
+        catList.append(newItem)
+    }
+
+    catButton.addEventListener("click", newFact)
 
 })
